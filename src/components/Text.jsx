1bb01fad8fd661a997280420a23f951f401ef13b
Abigail Agarin <abigail.agarin@iisysgroup.com>
2022-12-04 17:05:18 +0100

    classwork updated

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,23 +1,15 @@
-import React from 'react'
+import React from "react";
+// const Text = (props) => {
+//   const { tag, title } = props;
 
-const Text = (props) => {
-    const {tag, title} = props
+//   if (tag === "h1") {
+//     return <h1>{title}</h1>;
+//   }
 
-    if(tag === "h1") {
-        return <h1>{title}</h1>
-    }
+//   if (tag === "h2") {
+//     return <h2>{title}</h2>;
+//   }
+//   return <p>{title}</p>;
+// };
 
-    if(tag === "h2") {
-        return <h2>{title}</h2>
-    }
-  return (
-    <p>{title}</p>
-  )
-}
-
-export default Text
-
-
-
-
-
+export default Text;
